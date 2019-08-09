77497ba5c66cca50539fe7dcc7ec1dd054de039c
todd-finkler <tbfinkler@gmail.com>
2019-08-09 12:28:29 -0400

    yet another

--- outOfBounds.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	int a[3] = { 1, 2, 3 };
-	printf("%d\n", a[3]);
-	// no problem!
-
-}
--- signMixups.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	char name[10] = {0};
-	printf("Name: ");
-	read(0, name, 10);
-	printf("Hello %s!\n", name);
-}
